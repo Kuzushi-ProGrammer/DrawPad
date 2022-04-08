4a44e1ab904eceec1f69143969c6f9ea34d2b5a4
Dominique Spavor (Kuzushi) <derp.rosy@gmail.com>
2022-04-08 14:49:48 -0600

    -Removed Keyboard Module (Wasn't using it) -Experimenting with pygame-menu -MENU WORKS

--- Connect4/DrawingTest.py
+++ Connect4/DrawingTest.py
@@ -11,7 +11,7 @@
     # Controls
 
 # ---- Imports ---- #
-import pygame, sys, os, PIL, keyboard
+import pygame, sys, os, PIL, pygame_menu
 from pygame.locals import *
 from os import path
 from os.path import exists
@@ -51,10 +51,30 @@ pygame.display.set_caption('Drawing Test Program')      # Sets title of window
 screen = pygame.display.set_mode()                      # Creates a window for the game to be in
 dimensions = pygame.display.get_desktop_sizes()         # Grabs dimensions of desktop for window sizing
 
-# ---- S PRESSED FUNCTION ---- # MY FUCKING BRAIN HURTS AAA
+# ------ Resolution ------- #
+try:
+    dimensions = dimensions[0]                          # Accounts for dual monitors and single monitors
+except:
+    pass
+
+resolution = dimensions
+(windresx, windresy) = resolution
+
+#Don't actually need this for now
+#Leftside = (0, 0, 100, windresx - 50)                   # Position from left, position from top, width, height
+#Topside = (0, 0, windresx, 50)                          # Variables for drawing the bars
+#Rightside = (windresx - 50, 0, 50, windresy)
+#Bottomside = (0, windresy - 50, windresx, 50)
+
+# ---- Drawing Subsurface ---- #
+Drawingspace = (100, 50, windresx - 150, windresy - 100)# Rect
+Drawscreen = screen.subsurface(Drawingspace)            # Creates a subsurface that can be 'screenshotted' later
+Exportspace = pygame.Surface((windresx, windresy))
+Exportspace.blit(Drawscreen, Drawingspace)
+
+# ---- S PRESSED FUNCTION ---- #
 def keypress():
-    global n # GLOBAL VARIABLE S O M G   MSMNMNMM
-    pressedkey = False
+    global n
 
     # ---- Anti-Dupe ---- #
     exists = True
@@ -68,130 +88,116 @@ def keypress():
             break
 
     # ---- Exporting ---- #
-    pressedkey = True
-    while pressedkey == True:
 
-        print(os.path.exists(f'Exports/drawing{n}.jpg'))
-        Exportspace.blit(Drawscreen, Drawingspace)                                  # Also saves with left and top black bars (has something to do with the surface)
+    print(os.path.exists(f'Exports/drawing{n}.jpg'))
+    Exportspace.blit(Drawscreen, Drawingspace)                                  # Also saves with left and top black bars (has something to do with the surface)
             
-        if os.path.exists(f'Exports/drawing{n}.jpg') == True:                       # Currently overwrites files of same name (Not intentional)
-            n += 1
+    if os.path.exists(f'Exports/drawing{n}.jpg') == True:                       # Currently overwrites files of same name (Not intentional)
+        n += 1
 
-            pygame.image.save(Exportspace, f'Exports/drawing{n}.jpg')  
+        pygame.image.save(Exportspace, f'Exports/drawing{n}.jpg')  
 
-            img = Image.open(f'Exports/drawing{n}.jpg')
-            imgcrop = img.crop((100, 50, windresx - 50, windresy - 50))
-            imgcrop = imgcrop.save(f"Exports/drawing{n}.jpg")              
+        img = Image.open(f'Exports/drawing{n}.jpg')
+        imgcrop = img.crop((100, 50, windresx - 50, windresy - 50))
+        imgcrop = imgcrop.save(f"Exports/drawing{n}.jpg")              
 
-            print("Saved! (Path Exists)")
-            pressedkey = False
+        print("Saved! (Path Exists)")
+        pressedkey = False
 
-        elif os.path.exists(f'Exports/drawing{n}.jpg') == False:
-            pygame.image.save(Exportspace, f'Exports/drawing{n}.jpg')  
+    elif os.path.exists(f'Exports/drawing{n}.jpg') == False:
+        pygame.image.save(Exportspace, f'Exports/drawing{n}.jpg')  
 
-            img = Image.open(f'Exports/drawing{n}.jpg')
-            imgcrop = img.crop((100, 50, windresx - 50, windresy - 50))
-            imgcrop = imgcrop.save(f"Exports/drawing{n}.jpg") 
+        img = Image.open(f'Exports/drawing{n}.jpg')
+        imgcrop = img.crop((100, 50, windresx - 50, windresy - 50))
+        imgcrop = imgcrop.save(f"Exports/drawing{n}.jpg") 
 
-            print("Saved! (Path Doesn't Exist)")
-            pressedkey = False
-        else:
-            pressedkey = False
-            pass
-
-# ------ Resolution ------- #
-try:
-    dimensions = dimensions[0]                          # Accounts for dual monitors and single monitors
-except:
-    pass
-
-resolution = dimensions
-(windresx, windresy) = resolution
-
-# ---- Drawing Subsurface ---- #
-Drawingspace = (100, 50, windresx - 150, windresy - 100)# Rect
-Drawscreen = screen.subsurface(Drawingspace)            # Creates a subsurface that can be 'screenshotted' later
-Exportspace = pygame.Surface((windresx, windresy))
-Exportspace.blit(Drawscreen, Drawingspace)
-
-# ---- Initial Interface ---- #
-screen.fill(grey)
-pygame.draw.rect(screen, white, Drawingspace)
-
-#Don't actually need this for now
-#Leftside = (0, 0, 100, windresx - 50)                   # Position from left, position from top, width, height
-#Topside = (0, 0, windresx, 50)                          # Variables for drawing the bars
-#Rightside = (windresx - 50, 0, 50, windresy)
-#Bottomside = (0, windresy - 50, windresx, 50)
+        print("Saved! (Path Doesn't Exist)")
+       
+    else:
+        pass
 
 # ------------------------------------------------------------------------------- # MAIN LOOP
-running = True
-while running:
+def mainfunc():
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    screen.fill(grey)
+    pygame.draw.rect(screen, white, Drawingspace)
+
+    global colour
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
             
-    pygame.display.update()
+        pygame.display.update()
 
-    #pygame.draw.rect (screen, grey, Leftside)           # Left sidebar             
-    #pygame.draw.rect (screen, grey, Topside)            # Top sidebar
-    #pygame.draw.rect (screen, grey, Rightside)          # Right sidebar
-    #pygame.draw.rect (screen, grey, Bottomside)         # Bottom sidebar
+        #pygame.draw.rect (screen, grey, Leftside)           # Left sidebar             
+        #pygame.draw.rect (screen, grey, Topside)            # Top sidebar
+        #pygame.draw.rect (screen, grey, Rightside)          # Right sidebar
+        #pygame.draw.rect (screen, grey, Bottomside)         # Bottom sidebar
     
-# ---- Colour Swatches ---- #
-    pygame.draw.rect(screen, red, (25, 25, 50, 50))      # Red                      # Screen variable is the window
-    pygame.draw.rect(screen, green, (25, 100, 50, 50))   # Green                    # First bracket tuple is RGB value
-    pygame.draw.rect(screen, blue, (25, 175, 50, 50))    # Blue                     # Second bracket tuple is position
-    pygame.draw.rect(screen, white, (25, 250, 50, 50))   # White                    # !! (distance from left, distance from top, x size, y size) !! (position for rect render)
-    pygame.draw.rect(screen, black, (25, 325, 50, 50))   # Black
+        # ---- Colour Swatches ---- #
+        pygame.draw.rect(screen, red, (25, 25, 50, 50))      # Red                      # Screen variable is the window
+        pygame.draw.rect(screen, green, (25, 100, 50, 50))   # Green                    # First bracket tuple is RGB value
+        pygame.draw.rect(screen, blue, (25, 175, 50, 50))    # Blue                     # Second bracket tuple is position
+        pygame.draw.rect(screen, white, (25, 250, 50, 50))   # White                    # !! (distance from left, distance from top, x size, y size) !! (position for rect render)
+        pygame.draw.rect(screen, black, (25, 325, 50, 50))   # Black
     
-# ---- Mouse Tracking ---- #
-    mouse = pygame.mouse.get_pressed(num_buttons = 3)
-    mpos = pygame.mouse.get_pos()
+        # ---- Mouse Tracking ---- #
+        mouse = pygame.mouse.get_pressed(num_buttons = 3)
+        mpos = pygame.mouse.get_pos()
 
-    mposcoords = mpos
-    (mposx, mposy) = mposcoords
+        mposcoords = mpos
+        (mposx, mposy) = mposcoords
 
-# ---- Key Logic ---- #
+        # ---- Key Logic ---- #
 
-    key = pygame.key.get_pressed()
+        key = pygame.key.get_pressed()
     
-    if key[pygame.K_s]:
-        keypress()
+        if key[pygame.K_s]:
+            keypress()
 
-# ---- Colour Selection ---- #
-    if mposx >= 25 and mposx <= 75:
-        if mposy >= 25 and mposy <= 75 and mouse == (False, False, True):           # Red
-            colour = red
+        # ---- Colour Selection ---- #
+        if mposx >= 25 and mposx <= 75:
+            if mposy >= 25 and mposy <= 75 and mouse == (False, False, True):           # Red
+                colour = red
 
-        if mposy >= 100 and mposy <= 150 and mouse == (False, False, True):         # Green
-            colour = green
+            if mposy >= 100 and mposy <= 150 and mouse == (False, False, True):         # Green
+                colour = green
 
-        if mposy >= 175 and mposy <= 225 and mouse == (False, False, True):         # Blue
-            colour = blue
+            if mposy >= 175 and mposy <= 225 and mouse == (False, False, True):         # Blue
+                colour = blue
 
-        if mposy >= 250 and mposy <= 300 and mouse == (False, False, True):         # White
-            colour = white
+            if mposy >= 250 and mposy <= 300 and mouse == (False, False, True):         # White
+                colour = white
 
-        if mposy >= 325 and mposy <= 375 and mouse == (False, False, True):         # Black
-            colour = black
-# ---- Drawing Logic ---- #
-    try:
-        if mouse == (True, False, False):
-            if mposx  >= 100 and mposx <= windresx - 50 and mposy >= 50 and mposy <= windresy - 50:
-                pygame.draw.circle(screen, colour, mpos, 5)
-                pygame.display.update()
-    except ValueError:
-        pass
+            if mposy >= 325 and mposy <= 375 and mouse == (False, False, True):         # Black
+                colour = black
+        # ---- Drawing Logic ---- #
+        try:
+            if mouse == (True, False, False):
+                if mposx  >= 100 and mposx <= windresx - 50 and mposy >= 50 and mposy <= windresy - 50:
+                    pygame.draw.circle(screen, colour, mpos, 5)
+                    pygame.display.update()
+        except ValueError:
+            pass
    
-# ---- Canvas Wipe ---- #
-    if key[pygame.K_SPACE]:
-        pygame.draw.rect(screen, white, Drawingspace)
+        # ---- Canvas Wipe ---- #
+        if key[pygame.K_SPACE]:
+            pygame.draw.rect(screen, white, Drawingspace)
 
+# ---- Menu ---- #
+menusurf = pygame.Surface((windresx, windresy))
+menu = pygame_menu.Menu('Deez fucking nuts', windresx, windresy, theme = pygame_menu.themes.THEME_SOLARIZED)
 
-
-
+menu.add.text_input('Yes?: ', default = '')
+menu.add.button('Draw!', mainfunc)
+# menu.add.button('Controls', controlsfunc)
+menu.add.clock()
+menu.add.color_input('COLOUR SELECT!?!?!? LETS GOOOO', 'rgb')
+menu.mainloop(screen)
 
 
 pygame.quit()
